78e4fcb7123e869273ad811f53d027d26014cc15
magferati <magferatizannatul@gmail.com>
2026-01-04 19:36:19 +0600

    Remove .env from git tracking

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config  # for .env security
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+API_KEY =  config('API_KEY')
+SEARCH_ENGINE_ID = config('SEARCH_ENGINE_ID')          
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
